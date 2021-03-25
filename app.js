98e93e945e146835c40b0b1c9c23458b82102b16
AnnaVL2 <43822073+AnnaVL2@users.noreply.github.com>
2021-03-25 22:24:34 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const modal = document.querySelector('.modal');
 const previews = document.querySelectorAll('.gallery img');
+const previewsNarrow = document.querySelectorAll('.galleryNarrow img');
 const original = document.querySelector('.full-img');
 const caption = document.querySelector('.caption');
 
@@ -12,6 +13,18 @@ previews.forEach(preview => {
         const originalSrc = preview.getAttribute('data-original');
         original.src = originalSrc;
         const altText = preview.alt;
+        caption.textContent = altText;
+    });
+});
+
+previewsNarrow.forEach(previewN => {
+    previewN.addEventListener('click', () => {
+        modal.classList.add('open');
+        original.classList.add('open');
+        //Dynamic change text&image
+        const originalSrcN = previewN.getAttribute('data-original');
+        original.src = originalSrcN;
+        const altText = previewN.alt;
         caption.textContent = altText;
     });
 });
